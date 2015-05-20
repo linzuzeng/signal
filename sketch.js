0032a406a46d9b6cc9bf4483fa357086119475e8
Your Name <you@example.com>
2015-05-20 21:23:52 +0800

    mark

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 var mic, fft;
 
 function setup() {
-	createCanvas(1000,600);
+	createCanvas(1200,600);
 	noFill();
-	/*mic = new p5.Oscillator(1046.50);
+	/*mic = new p5.Oscillator(880);
 	mic.start();
 	mic.amp(1);
 
-		mic1= new p5.Oscillator(2093.00);
+		mic1= new p5.Oscillator(1760);
 		mic1.start();
 		mic1.amp(1);
 		mic1.connect(mic);
 		*/
 		mic2 = new p5.AudioIn();
+		mic2.amp(0.5);
 		mic2.start();
 		//mic2.connect(mic);
+		
 		fft = new p5.FFT(0,1024*8);
 		fft.setInput(mic2);
 	}
@@ -26,13 +28,13 @@ function setup() {
 
 		beginShape();
 
-		for (i = 80,a=0; (i)<(spectrum.length); i*=1.0028) {
+		for (i = 40,a=0; (i)<(spectrum.length); i*=1.0028) {
 			a++;
 			vertex(a, map(spectrum[Math.floor(i)], 0, 256, height, 0) );
 		}
 		endShape();
 		beginShape()
-		for (i = 80,a=0; (i)<(spectrum.length); i*=1.0028) {
+		for (i = 40,a=0; (i)<(spectrum.length); i*=1.0028) {
 			a++
 			switch (a%(62*4)) {
 				case (8+62):{
@@ -68,14 +70,13 @@ function setup() {
 
 			
 		}
-		str=["6","6#" ,"7" ,"1" ,"1#" ,"2" ,"2#" ,"3" ,"4" ,"4#", "5" ,"5#" ,"6","6#" ,"7" ,"1" ,"1#" ,"2" ,"2#" ,"3" ,"4" ,"4#", "5" ,"5#" ,"6","6#" ,"7" ,"1" ,"1#" ,"2" ,"2#" ,"3" ,"4" ,"4#", "5" ,"5#" ,"6","6#" ,"7" ,"1" ,"1#" ,"2" ,"2#" ,"3" ,"4" ,"4#", "5" ,"5#" ,"6"];
-
-		for (n=0;n<50;n++)
+		str=["6","6#" ,"7" ,"1" ,"1#" ,"2" ,"2#" ,"3" ,"4" ,"4#", "5" ,"5#" ];
+		for (n=0;n<60;n++)
 		{
 			a=8+248*n/12;
 				stroke("black");
 
-				text(str[n],a+3,30);
+				text(str[n%12],a+3,30);
 				stroke("white");
 				line(a,0,a,100  );
 			
