2d7f28f62b4018594b3175768d0a17b8ab9278df
Your Name <you@example.com>
2015-05-26 21:45:26 +0800

    new identify algorithm

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 var mic, osc, fft, song, capture = false,
   capture_t = 0;
+var last_three=[0,0,0];
 var round_last = "";
+var captured=piano;
 fft_f = 8192;
 low_f = 214.84375;
 length_f = 248;
-temp1 = [];
-temp2 = [];
-
-function mousePressed() {
-  capture = true;
+function checkthis(a){
+  if (last_three[3]!=a)
+  {
+    last_three[1]=last_three[2];
+    last_three[2]=last_three[3];
+    last_three[3]=a;
+    return (last_three[2]>last_three[1])&&(last_three[2]>last_three[3]);
+  }
 }
-
 function mulitply1(a, standard) {
   if (standard) {
     var total1 = 0,
@@ -22,12 +26,13 @@ function mulitply1(a, standard) {
       base1 += standard[i] * standard[i];
     for (var i = 0; i < a.length; i++)
       base2 += a[i] * a[i];
-    return (total1 * total1) / (base1 * base2);
+    return  {like:(total1 * total1) / (base1 * base2),amp:total1 / base1};
   } else {
-    return -1;
+    return {like:-1,amp:-1};
   }
 
 }
+
 
 function preload() {
   // Load a sound file
@@ -62,18 +67,30 @@ function setup() {
   createElement('h2', 'Homework for Signal & Systems - FFT spectrum analyzer');
   createElement('h', 'Input:');
   button = createButton('mic');
+  button.mousePressed(startstop);
+
   createElement('h', 'File:');
   createFileInput(handleFiles);
-  prompt = createElement('h', 'Click to set a standard signal: ');
-  recog = createElement('h', 'NaN');
+  prompt = createElement('h', 'Profile (DEF:piano): ');
+  button_capture = createButton("Capture")
+  createElement('br');
+  createElement('h', 'High-amp notes: ');
+  notes = createElement('h', ' (amp-method, may be incorrect) ');
+  createElement('br');
+  createElement('h', 'Identified notes: ');
+  recog = createElement('h', '--');
+  createElement('br');
+
+  button_capture.mousePressed(function(){
+    capture = !capture;
+  })
+
   createElement('br');
   createElement('br');
-  button.mousePressed(startstop);
   createCanvas(1200, 500);
   createElement('br');
   createElement('br');
-  createElement('h', 'Recognized notes: ');
-  notes = createElement('h', ' (amp-method, may be incorrect) ');
+
   noFill();
 
   /*
@@ -108,34 +125,16 @@ function draw() {
     spectrum_log[a] = 0;
     for (b = Math.floor(i / Math.pow(2, 1 / length_f)); b <= Math.floor(i); b++)
       if (spectrum[b] > spectrum_log[a])
-        spectrum_log[a] = Math.floor(spectrum[b] * spectrum[b] / 256);
+        spectrum_log[a] = spectrum[b] * spectrum[b] ;
   }
-  if (capture) {
-    capture = false;
-    switch (capture_t) {
-      case 0:
-        {
-          temp1 = spectrum_log;
-          prompt.html("Click to set the second standard signal:");
-          capture_t++;
-          break;
-        }
-      case 1:
-        {
-          temp2 = spectrum_log;
-          capture_t = 0;
-          prompt.html("Click to set a standard signal:");
-          break;
-        }
-    }
-  }
-  recog.html(Math.round(mulitply1(spectrum_log, temp1) * 100).toString() + "%	" + Math.round(mulitply1(spectrum_log, temp2) * 100).toString() + "%");
+
   beginShape();
   for (var a = 0; a < spectrum_log.length; a++) {
-    vertex(a, map(spectrum_log[a], 0, 256, height, 0));
+    vertex(a, map(spectrum_log[a], 0, 256*256, height, 0));
   }
   endShape();
 
+  //draw note
   var offset_f = Math.floor(Math.log(55 / low_f) / Math.log(Math.pow(2, 1 / length_f)));
   beginShape();
   var str = ["6", "6#", "7", "1", "1#", "2", "2#", "3", "4", "4#", "5", "5#"];
@@ -148,7 +147,7 @@ function draw() {
           stroke("black");
           text("1", a + 3, 10);
           stroke("blue");
-          line(a, 0, a, map(spectrum_log[a], 0, 256, height, 0));
+          line(a, 0, a, map(spectrum_log[a], 0, 256*256, height, 0));
           break;
         }
       case 0:
@@ -156,7 +155,7 @@ function draw() {
           stroke("black");
           text("A", a + 3, 10);
           stroke("red");
-          line(a, 0, a, map(spectrum_log[a], 0, 256, height, 0));
+          line(a, 0, a, map(spectrum_log[a], 0, 256*256, height, 0));
           break;
         }
     }
@@ -164,56 +163,72 @@ function draw() {
     text(str[n % 12], a + 3, 30);
     stroke("white");
     line(a, 0, a, 100);
-    //trigger!
-
-    var max_in_range = 0;
-    for (var bb = a - Math.floor(length_f / 12) - 1; bb < a + Math.floor(length_f / 12) + 1; bb++) {
-      if ((spectrum_log[a] > (256 *3 / 4))) {
-
-        if (spectrum_log[b - length_f] > 256 * 3 / 4)
-          break;
-        if (spectrum_log[b - length_f * 2] > 256 * 3 / 4)
-          break;
-
-        got_freq.add(n);
-
-        break;
-      }
-    }
   }
 
   endShape();
+  // amp trigger
   beginShape();
-  for (var a = 0; a < spectrum_log.length; a++) {
-
-    if (spectrum_log[a] > 256 * 3 / 4) {
+  for (var a = 0, n = 0; a < spectrum_log.length; n++, a = Math.floor(offset_f + length_f * n / 12)) {
+    for (var bb = a - Math.floor(length_f / 12) - 1; bb < a + Math.floor(length_f / 12) + 1; bb++) {
+      if ((spectrum_log[a] > (256 *256*3 / 4))) {
+        if (spectrum_log[bb - length_f] > 256 *256* 3 / 4)
+          break;
+        if (spectrum_log[bb - length_f * 2] > 256 *256* 3 / 4)
+          break;
+        got_freq.add(n);
+      }
+    }
+    if (spectrum_log[a] > 256 *256* 3 / 4) {
       stroke("red");
-      line(a, 0, a, map(spectrum_log[a], 0, 256, height, 0));
+      line(a, 0, a, map(spectrum_log[a], 0, 256*256, height, 0));
     }
   }
   if (got_freq.size==0)
-    {
+  {
       osc.amp(0);
-      //console.log("test");
-    }
+  }
   else{
-
-    osc.amp(1);
+    //osc.amp(1);
     var lowest = 214748;
     got_freq.forEach(function(n) {
       round_this += Math.floor(n / 12) + " [" + str[n % 12] + "] ";
-      //osc.start();
       if (lowest>n){
         lowest=n;
       }
-      //console.log(Math.round(440*Math.pow(2,(lowest-20)/12)));
       osc.freq(Math.round(440*Math.pow(2,(lowest-30)/12)));
-      //osc.freq(midiToFreq(n + 30));
     });
   }
-
   if ((round_last != round_this) )
     notes.html(round_this);
   round_last = round_this;
   endShape();
+
+  // capture
+
+  if (capture) {
+    if (checkthis(max(spectrum_log)))
+      {
+        captured[capture_t++]=spectrum_log;
+        console.log(capture_t);
+      }
+  }else {
+    if (checkthis(max(spectrum_log)))
+      {
+    var best=-1;
+    var best_id=0;
+    var best_amp=0;
+    for (var i=0; i<captured.length;i++){
+      var now =mulitply1(spectrum_log, captured[i]);
+      if (now.like>best)
+      {
+        best = now.like;
+        best_id=i;
+        best_amp=now.amp;
+      }
+    }
+    recog.html(best_id.toString() +"  ["+Math.round(best * 100).toString() + "%, "+Math.round(best_amp*100)+"]"  );
+  }
+  }
+  //recog.html(Math.round(mulitply1(spectrum_log, temp1) * 100).toString() + "%	" + Math.round(mulitply1(spectrum_log, temp2) * 100).toString() + "%");
+
 }
